88f3f0690cacf99c61d674393235a2c0aa3f68c0
Fastex007 <fastex007@yandex.ru>
2021-02-04 22:37:19 +0300

    Final 17 >__<

--- homework.py
+++ homework.py
@@ -12,7 +12,6 @@ import telegram
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
-print(os.environ)
 PRAKTIKUM_BASE_URL = (
     'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 )
